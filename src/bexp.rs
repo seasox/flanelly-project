ccda557190c41f42bc63faf40285ba530171eb5f
Jeremy Boy <github@jboy.eu>
2021-01-30 16:06:20 +0100

    implement BExp::fmt, BExp::sub_aexps for neg, and, or

--- src/bexp.rs
+++ src/bexp.rs
@@ -15,7 +15,10 @@ pub enum BExp {
 impl Display for BExp {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
-            BExp::LessEq(left, right) => {write!(f, "{} <= {}", left, right)}
+            BExp::LessEq(left, right) => { write!(f, "{} <= {}", left, right) },
+            BExp::Neg(b) => { write!(f, "!{}", b) },
+            BExp::And(left, right) => { write!(f, "{} && {}", left, right) },
+            BExp::Or(left, right) => { write!(f, "{} || {}", left, right) }
         }
     }
 }
@@ -26,6 +29,12 @@ impl BExp {
             BExp::LessEq(a1, a2) => {
                 // Rust Expl.: See also `AExp::sub_aexps` for a more detailed explanation 
                 a1.sub_aexps().union(&a2.sub_aexps()).cloned().collect()
+            }
+            BExp::Neg(b) => {
+                b.sub_aexps()
+            }
+            BExp::And(b1, b2) | BExp::Or(b1, b2) => {
+                b1.sub_aexps().union(&b2.sub_aexps()).cloned().collect()
             }
         }
     }
